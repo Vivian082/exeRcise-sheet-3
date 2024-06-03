9d8580ee652b5de8ae4b202cf3a28faf3fb245f7
Vivian082 <vivianyang2004@163.com>
2024-06-03 17:27:56 +0200

    Added strengths and weaknesses of Git

---
+++ exercise.3 Yang.R
@@ -0,0 +1,2 @@
+##Strengths:Version control: Git allows tracking changes and collaborating with others seamlessly.Branching and merging: Git supports multiple workflows with branches and can merge changes efficiently.
+##Weaknesses:Steep learning curve: Beginners might find Git commands and concepts complex.Merge conflicts: Resolving conflicts can be challenging, especially in large projects.
